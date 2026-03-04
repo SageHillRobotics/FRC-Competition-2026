fffcf4e4771817cdcdbab6ba65f933de46ee865f
Jahaan Pardhanani <kidflash2jahaan@gmail.com>
2026-03-04 14:58:55 -0800

    optimized turret

--- src/main/java/frc/robot/subsystems/CommandTurret.java
+++ src/main/java/frc/robot/subsystems/CommandTurret.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.LimelightHelpers;
@@ -12,6 +13,8 @@ public class CommandTurret extends SubsystemBase {
     private CommandSwerveDrivetrain drivetrain;
 
     private TalonFX turretMotor = new TalonFX(16);
+
+    private PIDController turretPID = new PIDController(0.01, 0, 0);
 
     public CommandTurret(CommandSwerveDrivetrain drivetrain) {
         this.drivetrain = drivetrain;
@@ -26,19 +29,17 @@ public class CommandTurret extends SubsystemBase {
     }
 
     public void poseEstimate() {
-        LimelightHelpers.SetRobotOrientation("limelight", LimelightHelpers.getIMUData("limelight").Yaw, 0, 0, 0, 0, 0);
+        LimelightHelpers.SetRobotOrientation("limelight", drivetrain.getPigeon2().getRotation2d().getDegrees(), 0, 0, 0, 0, 0);
         PoseEstimate poseEstimate = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2("limelight");
         drivetrain.setVisionMeasurementStdDevs(VecBuilder.fill(.7,.7,9999999));
         drivetrain.addVisionMeasurement(new Pose2d(poseEstimate.pose.getTranslation(), drivetrain.getPigeon2().getRotation2d()), poseEstimate.timestampSeconds);
     }
 
     public void trackTarget() {
-        if (LimelightHelpers.getFiducialID("limelight") != 10 && LimelightHelpers.getFiducialID("limelight") != 26) {
-            turretMotor.set(0);
-            return;
+        if (LimelightHelpers.getFiducialID("limelight") == 10 || LimelightHelpers.getFiducialID("limelight") == 26) {
+            turretMotor.set(turretPID.calculate(LimelightHelpers.getTX("limelight")));
+        } else {
+            turretMotor.set(turretPID.calculate(LimelightHelpers.getIMUData("limelight").Yaw, drivetrain.getPigeon2().getRotation2d().getDegrees()));
         }
-        double targetVelocity = LimelightHelpers.getTX("limelight") * -0.035;
-
-        turretMotor.set(targetVelocity);
     }
 }
